4be5c2db3541ea8cd8357e4b05809d52d609ced6
Andrei <test@mail.ru>
2023-06-04 00:37:17 +0800

    leasen_1

--- index.js
+++ index.js
@@ -2,4 +2,21 @@
 
 'use strict';
 
-// new 
+const numberOfFilms = +prompt('Сколько фильмов просмотрел', '');
+const PersonalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('один из последних просмотренных фильмов'),
+    b = prompt('На сколько оцените его?'),
+    c = prompt('Один из последных прссмотренных фильмов'),
+    d = prompt('На сколько оцените его?');
+
+PersonalMovieDB.movies[a] = b;
+PersonalMovieDB.movies[c] = d;
+
+console.log(PersonalMovieDB);
